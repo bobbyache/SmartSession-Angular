2940a2761bfd31a4e23919cdf21d2eefffad7816
RobBl8ke <RobB@korbicom.com>
2020-01-08 14:23:23 +0200

    Added dashboard tests to test links and the router

--- src/app/dashboard/dashboard.component.spec.ts
+++ src/app/dashboard/dashboard.component.spec.ts
@@ -1,4 +1,5 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { Location } from '@angular/common';
+import { async, ComponentFixture, TestBed, fakeAsync, flush } from '@angular/core/testing';
 
 import { DashboardComponent } from './dashboard.component';
 import { MaterialModule } from '../shared/material.module';
@@ -6,19 +7,38 @@ import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ApplicationSettingsService } from '../shared/services/application-settings.service';
 import { SnackbarService } from '../shared/services/snackbar/snackbar.service';
+import { By } from '@angular/platform-browser';
+import { Router } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { routes } from '../app-routing.module';
+import { RoutinesComponent } from '../routines/routines.component';
+import { RecorderComponent } from '../recorder/recorder.component';
+import { ExercisesComponent } from '../exercises/exercises.component';
+import { GoalsComponent } from '../goals/goals.component';
+import { DiaryComponent } from '../diary/diary.component';
 
 describe('DashboardComponent', () => {
   let component: DashboardComponent;
   let fixture: ComponentFixture<DashboardComponent>;
+  let location: Location;
+  let router: Router;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
+        RouterTestingModule.withRoutes(routes),
         MaterialModule,
         NoopAnimationsModule,
         HttpClientTestingModule
       ],
-      declarations: [ DashboardComponent ],
+      declarations: [
+        DashboardComponent,
+        RoutinesComponent,
+        RecorderComponent,
+        ExercisesComponent,
+        GoalsComponent,
+        DiaryComponent
+      ],
       providers: [
         ApplicationSettingsService,
         SnackbarService
@@ -27,13 +47,55 @@ describe('DashboardComponent', () => {
     .compileComponents();
   }));
 
-  beforeEach(() => {
+  beforeEach(fakeAsync(() => {
+    location = TestBed.get(Location);
+    router = TestBed.get(Router);
     fixture = TestBed.createComponent(DashboardComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    router.navigateByUrl('/');
+    advance();
+  }));
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  describe('when navigating', () => {
+    it('should navigate to successfully to recorder', fakeAsync(() => {
+      const menu = fixture.debugElement.queryAll(By.css('a[routerlink]'))[0];
+      menu.triggerEventHandler('click', { button: 0 });
+      advance();
+      expect(location.path()).toEqual('/recorder');
+    }));
+    it('should navigate to successfully to routines', fakeAsync(() => {
+      const menu = fixture.debugElement.queryAll(By.css('a[routerlink]'))[1];
+      menu.triggerEventHandler('click', { button: 0 });
+      advance();
+      expect(location.path()).toEqual('/routines');
+    }));
+    it('should navigate to successfully to exercises', fakeAsync(() => {
+      const menu = fixture.debugElement.queryAll(By.css('a[routerlink]'))[2];
+      menu.triggerEventHandler('click', { button: 0 });
+      advance();
+      expect(location.path()).toEqual('/exercises');
+    }));
+    it('should navigate to successfully to diary', fakeAsync(() => {
+      const menu = fixture.debugElement.queryAll(By.css('a[routerlink]'))[3];
+      menu.triggerEventHandler('click', { button: 0 });
+      advance();
+      expect(location.path()).toEqual('/diary');
+    }));
+    it('should navigate to successfully to goals', fakeAsync(() => {
+      const menu = fixture.debugElement.queryAll(By.css('a[routerlink]'))[4];
+      menu.triggerEventHandler('click', { button: 0 });
+      advance();
+      expect(location.path()).toEqual('/goals');
+    }));
+  });
+
+  function advance(): void {
+    flush();
+    fixture.detectChanges();
+  }
+
 });

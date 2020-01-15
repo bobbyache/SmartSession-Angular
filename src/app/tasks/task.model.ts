73d0de57de057c3e060ca6bd2c38a49b4ec0a1fd
RobBl8ke <RobB@korbicom.com>
2020-01-15 12:05:32 +0200

    Added ITaskSummary although it is not currently being used. Must learn a bit more about jsonserver

--- src/app/tasks/task.model.ts
+++ src/app/tasks/task.model.ts
@@ -1,3 +1,12 @@
+
+export interface ITaskSummary {
+    id: number;
+    title: string;
+    dateCreated: Date;
+    progress: number;
+}
+
+
 export interface ITask {
     id: number;
     title: string;
